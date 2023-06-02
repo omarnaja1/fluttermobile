3322eec66190db43d57c817c196631687feda431
Omar Naja <95374284+Theommy01@users.noreply.github.com>
2023-06-02 11:59:51 +0200

    Aggiunta FAB per geolocalizzazione (funzione da implementare)

--- SkiTracker/lib/main.dart
+++ SkiTracker/lib/main.dart
@@ -65,19 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
   var currentPage = DrawerSection.mappa;
   int _currentIndex = 0;
 
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -163,12 +150,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    */
+      */
 
 
       drawer: Drawer(
@@ -212,6 +194,14 @@ class _MyHomePageState extends State<MyHomePage> {
         },
       ),
 
+    /*    floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+         //  tooltip: 'Increment',
+          child: const Icon(Icons.location_on),
+        )
+
+
+     */
     );
   }
   Widget MyDrawerList(){
--- SkiTracker/lib/mappa.dart
+++ SkiTracker/lib/mappa.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Mappa extends StatelessWidget{
+
+  int _counter = 0;
+
+  void _geoLocation() {
+    // This call to setState tells the Flutter framework that something has
+    // changed in this State, which causes it to rerun the build method below
+    // so that the display can reflect the updated values. If we changed
+    // _counter without calling setState(), then the build method would not be
+    // called again, and so nothing would appear to happen.
+    _counter++;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Mappa"),
-      ),
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: _geoLocation,
+          //  tooltip: 'Increment',
+          child: const Icon(Icons.location_on),
+        )
     );
   }
 }
