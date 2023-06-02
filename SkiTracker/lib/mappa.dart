1049e42316a1b93ee0a8e9cd857adfd7ab42e747
Omar Naja <95374284+Theommy01@users.noreply.github.com>
2023-06-02 12:13:33 +0200

    Alcune modifiche al file mappa.dart

--- SkiTracker/lib/mappa.dart
+++ SkiTracker/lib/mappa.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Mappa extends StatelessWidget{
+class Mappa extends StatefulWidget {
+  const Mappa({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+
+  @override
+  State<Mappa> createState() => _MappaState();
+}
+
+class _MappaState extends State<Mappa> {
+  final Color titleColor = Colors.white;
 
   int _counter = 0;
 
@@ -15,12 +33,44 @@ class Mappa extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Mappa',
+            ),
+            Text(
+              'Qui verra visualizzata la mappa',
+            ),
+          ],
+        ),
+      ),
+
         floatingActionButton: FloatingActionButton(
           onPressed: _geoLocation,
-          //  tooltip: 'Increment',
+          tooltip: 'Increment',
           child: const Icon(Icons.location_on),
         )
     );
   }
+
 }
